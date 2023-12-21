aa579ba7b302e642b4d19130d3b9b6fd8fd7cb4d
Nabi <nabisaheb7780@gmail.com>
2023-12-21 12:51:20 +0530

    Expense poject setup

---
+++ main.tf
@@ -0,0 +1,21 @@
+terraform {
+  backend "s3" {
+    bucket = "nabi-terraform-state"
+    key    = "parameter-store/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
+variable "parameters" {
+  default = [
+    { name = "test", type = "string", value = "test" },
+    { name = "test1", type = "string", value = "test1" }
+  ]
+}
+
+resource "aws_ssm_parameter" "parameters" {
+  count = length(var.parameters)
+  name = var.parameters[count.index].name
+  type = var.parameters[count.index].type
+  value = var.parameters[count.index].value
+}
