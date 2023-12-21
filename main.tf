7df7102076aa99702b923b374143c208107d1c8b
Nabi <nabisaheb7780@gmail.com>
2023-12-21 13:28:40 +0530

    Expense poject setup

--- main.tf
+++ main.tf
@@ -15,7 +15,7 @@ variable "parameters" {
 
 resource "aws_ssm_parameter" "params" {
   count = length(var.parameters)
-  name = var.parameters[count.index].name
-  type = var.parameters[count.index].type
+  name  = var.parameters[count.index].name
+  type  = var.parameters[count.index].type
   value = var.parameters[count.index].value
 }
