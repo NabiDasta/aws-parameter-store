91e1947bf039a35c405af6feeb9712be1d034f2d
Nabi <nabisaheb7780@gmail.com>
2024-01-05 20:05:50 +0530

    Terraform

--- vars.tf
+++ vars.tf
@@ -3,6 +3,8 @@ variable "parameters" {
     { name = "test", type = "String", value = "test" },
     { name = "dev-rds.username", type = "String", value = "admin1" },
     { name = "dev.backend.DB_HOST", type = "String", value = "dev-mysql-rds.c3q862mmelzn.us-east-1.rds.amazonaws.com" },
+    { name = "dev.rds.endpoint", type = "String", value = "dev-mysql-rds.c3q862mmelzn.us-east-1.rds.amazonaws.com" },
+
 
     ### Passwords.
     # Passwords should never be kept under code, It always been created mostly manually / saperate automation where code is not involved.
