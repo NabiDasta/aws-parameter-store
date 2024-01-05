7e71e6d0d286e74a6706ab0dad80d9518fe9a0fe
Nabi <nabisaheb7780@gmail.com>
2024-01-05 22:09:38 +0530

    Terraform

--- vars.tf
+++ vars.tf
@@ -8,6 +8,6 @@ variable "parameters" {
 
     ### Passwords.
     # Passwords should never be kept under code, It always been created mostly manually / saperate automation where code is not involved.
-    { name = "dev-rds.password", type = "SecureString", value = "ExpenseApp1234" }
+    { name = "dev.rds.password", type = "SecureString", value = "ExpenseApp1234" }
   ]
 }
